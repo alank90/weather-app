8cd39370765453666f39aa0e23cbfb04fa4f1354
akillian@outlook.com <akillian@outlook.com>
2018-04-13 20:57:20 -0400

    First Commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Weather App' });
 });
 
 module.exports = router;
+
+// weather-app key - ffb27a3af8cae8c9cbf286a767e2df37
