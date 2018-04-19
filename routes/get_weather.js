81b68073355d4c39e7f820a0724371c3c515b738
Alan K <akillian@outlook.com>
2018-04-19 13:58:38 -0400

    commit

--- routes/get_weather.js
+++ routes/get_weather.js
@@ -8,7 +8,7 @@ const apiKey = "ffb27a3af8cae8c9cbf286a767e2df37";
 /* GET weather for a city. */
 router.post("/", function(req, res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}&type=like`;
 
   request(url, function(err, response, body) {
     if (err) {
@@ -16,6 +16,15 @@ router.post("/", function(req, res) {
     } else {
       let weather = JSON.parse(body);
       let weatherIcon = weather.weather[0].icon;
+      let currentConditions = weather.weather[0].description;
+      let humidity = weather.main.humidity;
+      let pressure = weather.main.pressure;
+      let temp_min = weather.main.temp_min;
+      let temp_max = weather.main.temp_max;
+      let wind = weather.wind.speed;
+      let wind_direction = weather.wind.deg;
+
+      console.log(weather.name);
 
       if (weather.main == undefined) {
         res.render("index", {
@@ -23,13 +32,24 @@ router.post("/", function(req, res) {
           error: "Error, please try again"
         });
       } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${
+       /*  let weatherText = `It's ${weather.main.temp} degrees in ${
           weather.name
-        } and the current conditions are ${weather.weather[0].description} !`;
-         res.render("get_weather", { weather: weatherText, weather_icon: weatherIcon, error: null });
+        } and the current conditions are ${weather.weather[0].description} !`; */
+        res.render("get_weather", {
+          weather: weather,
+          city: weather.name,
+          weather_icon: weatherIcon,
+          temp: weather.main.temp,
+          currentConditions: weather.weather[0].description,
+          humidity: weather.main.humidity,
+          pressure: weather.main.pressure,
+          wind:  weather.wind.speed,
+          wind_direction: weather.wind.deg,
+          error: null
+        });
       }
     }
   });
 });
- 
+
 module.exports = router;
