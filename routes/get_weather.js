c6da12cd9d99cb2056041f97365195605e35bd79
Alan K <akillian@outlook.com>
2018-04-20 13:51:57 -0400

    commit

--- routes/get_weather.js
+++ routes/get_weather.js
@@ -41,14 +41,3 @@ router.post("/", function(req, res) {
 });
 
 module.exports = router;
-
-
-/* Module to convert degrees to compass directions 
-
-function degToCompass(num) {
-  var val = Math.floor((num / 22.5) + 0.5);
-  var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
-  return arr[(val % 16)];
-}
-
-*/
